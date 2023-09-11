c087ccebbe8aa4c353a609e528b5407c30870b27
MasrulSakib <Masrul.Sakib69@gmail.com>
2023-09-11 18:06:53 +0600

    navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className="header-container">
+
+            <div className='logo'>
+                <p className='logo-type'><FontAwesomeIcon icon={faDumbbell} /></p>
+                <span className="header-text">Strength</span>
+                <span className="header-text2">Sphere</span>
+            </div>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/login">Login</a>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Header;
